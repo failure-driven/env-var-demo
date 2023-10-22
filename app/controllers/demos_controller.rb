9800c244565f4583b568c2a711dcf61d5dbe7a57
Michael Milewski <saramic@gmail.com>
2023-10-22 11:30:29 +1100

    [🛤️ 🫕 ] show env vars and files at rest in rails root page

---
+++ app/controllers/demos_controller.rb
@@ -0,0 +1,25 @@
+class DemosController < ApplicationController
+  def index
+    @env_vars = {}
+    %w[
+      EJSON_ENCRPYTED
+      _EJSON_VISIBLE
+      ANSIBLE_VAULT_ENCRYPTED_ENV
+      ANSIBLE_VAULT_VISIBLE_ENV
+    ].each do |env_var_key|
+      @env_vars[env_var_key] = ENV.fetch(env_var_key, nil)
+    end
+  end
+
+  def read_file
+    respond_to do |format|
+      format.html do
+        content = File
+          .read(Rails.root.join(params[:filename]))
+          .split("\n")
+          .map{ |line| "<code>#{line}</code><br />" }
+        render :inline => content.join("\n").html_safe
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resource :demos do
+    collection do
+      get :read_file
+    end
+  end
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "demos#index"
 end
